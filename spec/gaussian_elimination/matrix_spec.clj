c2a8006cb0057a5f0aec96966e7297156e8ba7e8
Wai Lee <skatenerd@gmail.com>
2012-01-29 13:35:11 -0600

    build repo

---
+++ spec/gaussian_elimination/matrix_spec.clj
@@ -0,0 +1,32 @@
+(ns gaussian_elimination.matrix_spec
+  (use [gaussian_elimination.matrix]
+       [speclj.core]))
+(describe "matrix stuff"
+  (with cur-matrix [[64 8 1]
+                    [49 7 1]
+                    [36 6 1]])
+  (it "swaps correctly"
+   (let [swapped (swap @cur-matrix 0 1)
+         desired [[49 7 1]
+                  [64 8 1]
+                  [36 6 1]]]
+     (should= desired swapped)))
+  (it "replaces correcly"
+    (let [replaced (replace-row @cur-matrix 0 (list 1 1 1))
+          desired [[1 1 1]
+                   [49 7 1]
+                   [36 6 1]]]
+      (should= desired replaced)))
+  (it "gets rows"
+      (should= [64 8 1] (get-row @cur-matrix 0)))
+  (it "gets cols"
+      (should= [64 49 36] (get-col @cur-matrix 0)))
+  (it "inserts cols"
+    (let [final-col [9 9 9]
+          appended (add-col @cur-matrix final-col)]
+      (should= [9 9 9] (get-col appended 3))
+      (should= [64 49 36] (get-col appended 0))))
+)
+
+
+(run-specs)
---
+++ src/gaussian_elimination/gaussian.clj
@@ -0,0 +1,82 @@
+(ns gaussian_elimination.gaussian
+  (use [gaussian_elimination.matrix])
+  (use [gaussian_elimination.vector])
+  (use [clojure.contrib.math]))
+(declare pivot triangulate)
+
+(defn gaussian-elimination [m evaluations]
+  22)
+
+(defn eliminate [pivot to-trim column]
+  (let [pivot-elt (nth pivot column)
+        to-trim-elt (nth to-trim column)
+        scaling-factor (/ to-trim-elt pivot-elt)
+        scaled (scale-vector pivot scaling-factor)
+        with-column-eliminated (subtract-vectors to-trim scaled)
+        with-zero-enforced (assoc 
+                             (vec with-column-eliminated)
+                             column
+                             0)]
+    with-zero-enforced))
+
+
+(defn row-at-bottom [matrix pivot-row-idx]
+  (>= (inc pivot-row-idx) (count matrix)))
+
+
+(defn safe-append [first second]
+  (if (empty? second)
+    first
+    (apply conj (vec first) (vec second))))
+
+(defn eliminate-col [matrix pivot-row-idx col-idx]
+  (if (row-at-bottom matrix pivot-row-idx)
+    matrix
+    (let [untouched-rows (take (inc pivot-row-idx) matrix)
+          to-mutate (drop (inc pivot-row-idx) matrix)
+          pivot (nth matrix pivot-row-idx)
+          mutated (for [row to-mutate]
+                    (eliminate
+                      pivot
+                      row
+                      col-idx))]
+      (safe-append untouched-rows mutated))))
+
+(defn iter-triangulation [triangulated remaining col-idx]
+  (let [pivoted-remaining (pivot
+                            remaining
+                            0
+                            col-idx)
+        mutated (eliminate-col
+                  pivoted-remaining
+                  0
+                  col-idx)
+        new-col-idx (inc col-idx)]
+    (triangulate
+      (conj triangulated
+            (first mutated))
+      (drop 1 mutated)
+      new-col-idx)))
+
+(defn triangulate
+  ([matrix] (triangulate [] matrix 0))
+  ([triangulated remaining col-idx]
+   (if (empty? remaining)
+     triangulated
+     (iter-triangulation triangulated remaining col-idx))))
+
+(defn idx-of-max-val [v]
+  (apply
+    max-key
+    #(abs (nth v %))
+    (range (count v))))
+
+(defn pivot [matrix pivot-row-idx pivot-col-idx]
+  (if (empty? matrix)
+    matrix
+    (let [relevant-col (get-col matrix pivot-col-idx)
+          row-with-max-abs-val (idx-of-max-val relevant-col)]
+      (swap matrix pivot-row-idx row-with-max-abs-val))))
+(defn back-substitute
+  ([m] (back-substitute m []))
+  ([m answers-so-far] 22))
