4b04e78af42ecdf8846eec712bf6be47be4bc483
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-01-30 10:25:09 -0600

    screw with namespacing to make compatible with java

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gaussian_elimination "1.0.0"
+(defproject gaussian.elimination "1.0.0"
   :description "Gaussian Elimination Implementation"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
