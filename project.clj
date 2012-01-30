358e9e9cb3c8c6f40b4684550c854874f3860173
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-01-30 09:10:44 -0600

    update project file

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gaussian_elimination "1.0.0-SNAPSHOT"
+(defproject gaussian_elimination "1.0.0"
   :description "Gaussian Elimination Implementation"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
